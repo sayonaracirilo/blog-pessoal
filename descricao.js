51980508cb5428c52019b88ffd65993a32b364cf
sayonara <sayonaracirilo10@hotmail.com>
2020-05-01 22:57:09 -0300

    update

--- descricao.js
+++ descricao.js
@@ -6,25 +6,20 @@ var emailElement = document.querySelector('#email')
 //consumindo a api pública do GitHub para coletar o e-mail, foto do perfil e nome
 //usando a bib axios para fazer a requisicao
 
-axios
-  .get('https://api.github.com/users/sayonaracirilo', {
-    headers: { Authorization: 'token 6991f494cffbf20c595f671b1686eabd2efdbe09' },
-  })
-  .then(function (response) {
-    renderInfo(response.data)
-    console.log(response.data)
-  })
-
-console.log(response)
+axios.get('https://api.github.com/users/sayonaracirilo/events/public').then(function (response) {
+  renderInfo(response.data[0])
+})
 
 function renderInfo(response) {
-  console.log(response.avatar_url)
-  avatarElement.setAttribute('src', '' + response.avatar_url + '')
+  var author = response.payload.commits[0].author
 
-  emailElement.innerText = '' + response.email + ''
-  nome.innerText = '' + response.name + ''
+  avatarElement.setAttribute('src', '' + response.actor.avatar_url + '')
+
+  emailElement.innerText = '' + author.email + ''
+  nomeElement.innerText = '' + author.name + ''
 
   console.log(emailElement)
+  console.log(nomeElement)
 }
 
 renderInfo()
